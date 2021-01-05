8938f363aa7a591dcab3b674bbfee3181351dca2
AALEKH1111 <72079790+AALEKH1111@users.noreply.github.com>
2021-01-05 19:16:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,8 +5,8 @@ var database;
 
 function preload(){
   //load images here
-  dogImg = loadImage("images/dogImg.png");
-  dogImg1 = loadImage("images/dogImg1.png");
+  dogImg = loadImage("dogImg.png");
+  dogImg1 = loadImage("dogImg1.png");
 }
 
 function setup() {
